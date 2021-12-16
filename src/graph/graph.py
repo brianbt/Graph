16218de1446079c7acaf1da799e0c8a72b45b3d3
Brian <brianchan.xd@gmail.com>
2021-12-16 15:14:53 +0800

    testing

--- src/graph/graph.py
+++ src/graph/graph.py
@@ -1,4 +1,3 @@
-import graph_algo
 import numpy as np
 
 class Graph:
@@ -9,6 +8,11 @@ class Graph:
         size (int): number of nodes in the graph
     
     Usages:
+        from graph import Graph
+        import graph_algo
+        import numpy as np
+
+        # Construct Graph
         g = Graph(5)
         g.add_Dedge(0, 1)
         g.add_Dedge(0, 2, 10)
@@ -17,12 +21,16 @@ class Graph:
         g.add_Dedge(2, 4, 3)
         g.add_Dedge(3, 4)
         g.add_Dedge(4, 0)
-        print(g)
-        print(g.get_adj_vertices(0))
+        print(g)    # for printing the graph in a neat way
+        print(g.get_adj_vertices(0))    #get all vertices directly reachable from node 0
+
+        # Graph Traversal
         print(graph_algo.BFS(g, 0, unweight=True))
         print(graph_algo.DFS(g, 0, unweight=True))
         print(graph_algo.DFS_recursive(g, 0))
         print(graph_algo.get_order(g, 0, 2, "bellmanFord"))
+
+        # Shortest Path
         true_dist = [[0,1,10,11,12],[np.inf,0,1,2,3],[np.inf,np.inf,0,1,2],[np.inf,np.inf,np.inf,0,1],[np.inf,np.inf,np.inf,np.inf,0]]
         a_star_d = graph_algo.a_star(g, true_dist, 0, 4)
         print(a_star_d)
